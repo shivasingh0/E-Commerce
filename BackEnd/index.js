253c3f86bb5f2d7837ac2f7ea22184e75e1c2606
shivasingh0 <shiva2003rajawat@gmail.com>
2023-09-26 23:46:25 +0530

    Eight Commit

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const connectToDatabase = require("./db/config");
 const User = require("./db/users");
-const Product = require('./db/product')
+const Product = require("./db/product");
+
+// import jwt token
+const Jwt = require("jsonwebtoken");
+const jwtkey = "e-commerce";
+
 const app = express();
 
 // Database connection
@@ -16,8 +21,14 @@ const register = async (req, res) => {
   let newUser = new User(req.body);
   let result = await newUser.save();
   result = result.toObject();
-  delete result.password
-  res.send(result);
+  delete result.password;
+  Jwt.sign({ result }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+    if (err) {
+      res.send({ result: "Something went Wrong try after sometimes..." });
+    }
+
+    res.send({ result, auth: token });
+  });
 };
 app.post("/register", register);
 
@@ -26,8 +37,15 @@ const login = async (req, res) => {
   if (req.body.email && req.body.password) {
     let user = await User.findOne(req.body).select("-password");
 
+    // using JWt token here
     if (user) {
-      res.send(user);
+      Jwt.sign({ user }, jwtkey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+          res.send({ result: "Something went Wrong try after sometimes..." });
+        }
+
+        res.send({ user, auth: token });
+      });
     } else {
       res.send({ result: "User not Found" });
     }
@@ -38,70 +56,70 @@ const login = async (req, res) => {
 app.post("/login", login);
 
 //  Product - API
-const product = async(req, res) => {
+const product = async (req, res) => {
   let newProduct = new Product(req.body);
-  let result = await newProduct.save()
+  let result = await newProduct.save();
   res.send(result);
-}
-app.post('/add-product', product)
+};
+app.post("/add-product", product);
 
 //  Display Product API
-const displayProduct = async(req,res) =>{
+const displayProduct = async (req, res) => {
   let products = await Product.find();
   if (products.length > 0) {
-    res.send(products)
+    res.send(products);
   } else {
-    res.send({ result : 'No Product Found' })
+    res.send({ result: "No Product Found" });
   }
-}
-app.get('/products',displayProduct)
+};
+app.get("/products", displayProduct);
 
 // Delete Product API
-const deleteProduct = async (req, res) =>{
-  const result = await Product.deleteOne({_id: req.params.id})
-  res.send(result)
-}
-app.delete('/products/:id', deleteProduct)
+const deleteProduct = async (req, res) => {
+  const result = await Product.deleteOne({ _id: req.params.id });
+  res.send(result);
+};
+app.delete("/products/:id", deleteProduct);
 
 //  Update Product API
 const updateProduct = async (req, res) => {
-  const result = await Product.findOne({_id:req.params.id})
+  const result = await Product.findOne({ _id: req.params.id });
   if (result) {
-    res.send(result)
-  } else{
-    res.send({result : 'Record not found'})
+    res.send(result);
+  } else {
+    res.send({ result: "Record not found" });
   }
-}
-app.get('/products/:id',updateProduct)
+};
+app.get("/products/:id", updateProduct);
 
 //  Update existing product API
-const updateExistingProduct = async (req,res) => {
+const updateExistingProduct = async (req, res) => {
   let result = await Product.updateOne(
-    {_id: req.params.id},
+    { _id: req.params.id },
     {
-      $set : req.body     // $set is used to set update data
-    })
-    res.send(result);
-}
-app.put('/products/:id', updateExistingProduct)
+      $set: req.body, // $set is used to set update data
+    }
+  );
+  res.send(result);
+};
+app.put("/products/:id", updateExistingProduct);
 
 // Search Product API
 const searchProduct = async (req, res) => {
   try {
     let result = await Product.find({
-      "$or" : [
-        {name: {$regex: req.params.key}},
-        {company: {$regex: req.params.key}},
-        {category: {$regex: req.params.key}}
-      ]
+      $or: [
+        { name: { $regex: req.params.key } },
+        { company: { $regex: req.params.key } },
+        { category: { $regex: req.params.key } },
+      ],
     });
     res.send(result);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
   }
-}
-app.get('/search/:key', searchProduct)
-
+};
+app.get("/search/:key", searchProduct);
 
 app.listen(5000);
